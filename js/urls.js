835008ed14e9a8ba73e0e4adbe0fe4a39e9be043
Alexis Castillo <alexis.castillo.pimienta@gmail.com>
2022-05-05 22:06:48 -0600

    Se empieza a trabajar con js

---
+++ js/urls.js
@@ -0,0 +1,249 @@
+// Colocar el URL despues de 'https://www.youtube.com/embed/'
+ytT1 = [
+    {
+        "Id": 0,
+        "Nombre": 'Piloto',
+        "URL": 'JZ-d--REspY'
+    },
+    {
+        "Id": 1,
+        "Nombre": 'IngenieriaDeProcesos',
+        "URL": 'RfnbzdJrPgc'
+    },
+    {
+        "Id": 2,
+        "Nombre": 'El Proceso de Salir de tu Zona de Confort',
+        "URL": 'Udfs4OXb4M4'
+    },
+    {
+        "Id": 3,
+        "Nombre": '¿Profesionales de la Comunicación e Ing. de Procesos?',
+        "URL": 'uLyoenmoWKI'
+    },
+    {
+        "Id": 4,
+        "Nombre": 'DMAIC - Definir',
+        "URL": 'AUltdlUloPo'
+    },
+    {
+        "Id": 5,
+        "Nombre": 'DMAIC - Medir',
+        "URL": 'KkUKCTg6590'
+    },
+    {
+        "Id": 6,
+        "Nombre": 'DMAIC - Analizar',
+        "URL": 's72f0bTgouo'
+    },
+    {
+        "Id": 7,
+        "Nombre": 'DMAIC - Mejorar',
+        "URL": 'GiU5Tx8s2dg'
+    },
+    {
+        "Id": 8,
+        "Nombre": 'DMAIC - Control',
+        "URL": 'y5L1sWClsec'
+    },
+    {
+        "Id": 9,
+        "Nombre": 'Hablemos de Industria 4.0',
+        "URL": 'Mx7LSLZC0qk'
+    },
+    {
+        "Id": 10,
+        "Nombre": "Indicadores de Desempeño (KPI's)",
+        "URL": '3bdew0jLHEw'
+    },
+    {
+        "Id": 11,
+        "Nombre": '¿El Límite de la Mejora Continua? (Costo-Tiempo-Calidad)',
+        "URL": 'bJ9dW-s9PSU'
+    },
+    {
+        "Id": 12,
+        "Nombre": 'Finanzas Personales / Empresariales y Mejora Continua',
+        "URL": 'JsFhfaKB_6I'
+    },
+    {
+        "Id": 13,
+        "Nombre": 'Liderazgo y Mejora Continua',
+        "URL": '5zA9lq91mNM'
+    },
+    {
+        "Id": 14,
+        "Nombre": 'Logística y Cadena de Suministro',
+        "URL": 'huPfuJ-3J2k'
+    },
+    {
+        "Id": 15,
+        "Nombre": 'Las 7 Herramientas Básicas de Calidad',
+        "URL": 'SJ31aTY0GKI'
+    },
+    {
+        "Id": 16,
+        "Nombre": 'Administración de Inventario',
+        "URL": 'elLW0BI46b0'
+    },
+    {
+        "Id": 17,
+        "Nombre": 'Tips para Conseguir tu Primer Empleo',
+        "URL": 'QOcIp0N-9Fg'
+    },
+    {
+        "Id": 18,
+        "Nombre": 'Los 8 Desperdicios',
+        "URL": '5ykid9e1kkU'
+    },
+    {
+        "Id": 19,
+        "Nombre": 'Inteligencia Artificial',
+        "URL": 'TY5pGElsob0'
+    }
+    {
+        "Id": 20,
+        "Nombre": '4 Libros de Mejora Continua',
+        "URL": 'aYocXZNo5Pw'
+    },
+]
+
+ytT2 = [
+    {
+        "Id": 1,
+        "URL": 'e5Dc6mhKUFo',
+    },
+    {
+        "Id": 2,
+        "URL": 'qEMkX5ieaB8',
+    },
+    {
+        "Id": 3,
+        "URL": '7yP9Z-8GKNY',
+    },
+    {
+        "Id": 4,
+        "URL": 'EU2PmVilh0k'
+    },
+    {
+        "Id": 5,
+        "URL": 'ouWZPZYutAQ',
+    },
+    {
+        "Id": 6,
+        "URL": '1hCjostIoJU'
+    },
+    {
+        "Id": 7,
+        "URL": 'H-LbygPnOFM',
+    },
+]
+
+// Colocar el URL despues de 'https://open.spotify.com/embed/episode/'
+// y antes de '?utm_source=generator&theme=0'
+spT1 = [
+    {
+        "Id": 0,
+        "Nombre": 'Piloto',
+        "URL": '2KKK6W4bslPNveAzIAUetz',
+    },
+    {
+        "Id": 1,
+        "Nombre": 'Ingeniería de Procesos',
+        "URL": '5uc2DQxvxS7NfsDNrB7B8i',
+    },
+    {
+        "Id": 2,
+        "Nombre": 'El Proceso de Salir de tu Zona de Confort',
+        "URL": '5c3stOhy7C1I4cRfHlp87A',
+    },
+    {
+        "Id": 3,
+        "Nombre": '¿Profesionales de la Comunicación e Ing. de Procesos?',
+        "URL": '3QOfloYy2HaAnfu9Qsz5yP',
+    },
+    {
+        "Id": 4,
+        "Nombre": 'DMAIC - Definir',
+        "URL": '',
+    },
+    {
+        "Id": 5,
+        "Nombre": 'DMAIC - Medir',
+        "URL": '',
+    },
+    {
+        "Id": 6,
+        "Nombre": 'DMAIC - Analizar',
+        "URL": '',
+    },
+    {
+        "Id": 7,
+        "Nombre": 'DMAIC - Mejorar',
+        "URL": '',
+    },
+    {
+        "Id": 8,
+        "Nombre": 'DMAIC - Control',
+        "URL": '',
+    },
+    {
+        "Id": 9,
+        "Nombre": 'Hablemos de Industria 4.0',
+        "URL": '',
+    },
+    {
+        "Id": 10,
+        "Nombre": "Indicadores de Desempeño (KPI's)",
+        "URL": '',
+    },
+    {
+        "Id": 11,
+        "Nombre": '¿El Límite de la Mejora Continua? (Costo-Tiempo-Calidad)',
+        "URL": '',
+    },
+    {
+        "Id": 12,
+        "Nombre": 'Finanzas Personales / Empresariales y Mejora Continua',
+        "URL": '',
+    },
+    {
+        "Id": 13,
+        "Nombre": 'Liderazgo y Mejora Continua',
+        "URL": '',
+    },
+    {
+        "Id": 14,
+        "Nombre": 'Logística y Cadena de Suministro',
+        "URL": '',
+    },
+    {
+        "Id": 15,
+        "Nombre": 'Las 7 Herramientas Básicas de Calidad',
+        "URL": '',
+    },
+    {
+        "Id": 16,
+        "Nombre": 'Administración de Inventario',
+        "URL": '',
+    },
+    {
+        "Id": 17,
+        "Nombre": 'Tips para Conseguir tu Primer Empleo',
+        "URL": '',
+    },
+    {
+        "Id": 18,
+        "Nombre": 'Los 8 Desperdicios',
+        "URL": '',
+    },
+    {
+        "Id": 19,
+        "Nombre": 'Inteligencia Artificial',
+        "URL": '',
+    },
+    {
+        "Id": 20,
+        "Nombre": '4 Libros de Mejora Continua',
+        "URL": '',
+    }
+]
