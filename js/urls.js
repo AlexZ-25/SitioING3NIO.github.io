a1fd073dfaa0caf772f1353da16f0ced2a71c506
Alexis Castillo <alexis.castillo.pimienta@gmail.com>
2022-05-07 23:47:06 -0600

    Se comienza a trabajar con spotify dinamico

--- js/urls.js
+++ js/urls.js
@@ -99,151 +99,203 @@ ytT1 = [
         "Id": 19,
         "Nombre": 'Inteligencia Artificial',
         "URL": 'TY5pGElsob0'
-    }
+    },
     {
         "Id": 20,
         "Nombre": '4 Libros de Mejora Continua',
         "URL": 'aYocXZNo5Pw'
-    },
-]
-
+    }
+];
 ytT2 = [
     {
         "Id": 1,
+        "Nombre": 'Capital Humano: Tendencias y Mejora Continua',
         "URL": 'e5Dc6mhKUFo',
     },
     {
         "Id": 2,
+        "Nombre": "Estudiar o No Una Maestría",
         "URL": 'qEMkX5ieaB8',
     },
     {
         "Id": 3,
+        "Nombre": 'Control de Almacén Vs Control de Inventario',
         "URL": '7yP9Z-8GKNY',
     },
     {
         "Id": 4,
+        "Nombre": 'Todos Debemos de Aprender a Programar',
         "URL": 'EU2PmVilh0k'
     },
     {
         "Id": 5,
+        "Nombre": 'Logística y Cadena de Suministro con Lucy González',
         "URL": 'ouWZPZYutAQ',
     },
     {
         "Id": 6,
+        "Nombre": 'Hablemos de RPA',
         "URL": '1hCjostIoJU'
     },
     {
         "Id": 7,
+        "Nombre": 'Hablemos de Blockchain',
         "URL": 'H-LbygPnOFM',
-    },
-]
+    }
+];
 
 // Colocar el URL despues de 'https://open.spotify.com/embed/episode/'
 // y antes de '?utm_source=generator&theme=0'
-spT1 = [
+sp = [
     {
-        "Id": 0,
+        "Id": 'T1E0',
         "Nombre": 'Piloto',
         "URL": '2KKK6W4bslPNveAzIAUetz',
     },
     {
-        "Id": 1,
+        "Id": 'T1E1',
         "Nombre": 'Ingeniería de Procesos',
         "URL": '5uc2DQxvxS7NfsDNrB7B8i',
     },
     {
-        "Id": 2,
+        "Id": 'T1E2',
         "Nombre": 'El Proceso de Salir de tu Zona de Confort',
         "URL": '5c3stOhy7C1I4cRfHlp87A',
     },
     {
-        "Id": 3,
+        "Id": 'T1E3',
         "Nombre": '¿Profesionales de la Comunicación e Ing. de Procesos?',
         "URL": '3QOfloYy2HaAnfu9Qsz5yP',
     },
     {
-        "Id": 4,
+        "Id": 'T1E4',
         "Nombre": 'DMAIC - Definir',
         "URL": '',
     },
     {
-        "Id": 5,
+        "Id": 'T1E5',
         "Nombre": 'DMAIC - Medir',
         "URL": '',
     },
     {
-        "Id": 6,
+        "Id": 'T1E6',
         "Nombre": 'DMAIC - Analizar',
         "URL": '',
     },
     {
-        "Id": 7,
+        "Id": 'T1E7',
         "Nombre": 'DMAIC - Mejorar',
         "URL": '',
     },
     {
-        "Id": 8,
+        "Id": 'T1E8',
         "Nombre": 'DMAIC - Control',
         "URL": '',
     },
     {
-        "Id": 9,
+        "Id": 'T1E9',
         "Nombre": 'Hablemos de Industria 4.0',
         "URL": '',
     },
     {
-        "Id": 10,
+        "Id": 'T1E10',
         "Nombre": "Indicadores de Desempeño (KPI's)",
         "URL": '',
     },
     {
-        "Id": 11,
+        "Id": 'T1E11',
         "Nombre": '¿El Límite de la Mejora Continua? (Costo-Tiempo-Calidad)',
         "URL": '',
     },
     {
-        "Id": 12,
+        "Id": 'T1E12',
         "Nombre": 'Finanzas Personales / Empresariales y Mejora Continua',
         "URL": '',
     },
     {
-        "Id": 13,
+        "Id": 'T1E13',
         "Nombre": 'Liderazgo y Mejora Continua',
         "URL": '',
     },
     {
-        "Id": 14,
+        "Id": 'T1E14',
         "Nombre": 'Logística y Cadena de Suministro',
         "URL": '',
     },
     {
-        "Id": 15,
+        "Id": 'T1E15',
         "Nombre": 'Las 7 Herramientas Básicas de Calidad',
         "URL": '',
     },
     {
-        "Id": 16,
+        "Id": 'T1E16',
         "Nombre": 'Administración de Inventario',
         "URL": '',
     },
     {
-        "Id": 17,
+        "Id": 'T1E17',
         "Nombre": 'Tips para Conseguir tu Primer Empleo',
         "URL": '',
     },
     {
-        "Id": 18,
+        "Id": 'T1E18',
         "Nombre": 'Los 8 Desperdicios',
         "URL": '',
     },
     {
-        "Id": 19,
+        "Id": 'T1E19',
         "Nombre": 'Inteligencia Artificial',
         "URL": '',
     },
     {
-        "Id": 20,
+        "Id": 'T1E20',
         "Nombre": '4 Libros de Mejora Continua',
         "URL": '',
+    },
+    {
+        "Id": 'T2E1',
+        "Nombre": 'Capital Humano: Tendencias y Mejora Continua',
+        "URL": '6GrLVySyv4fNb0291spTHP',
+    },
+    {
+        "Id": 'T2E2',
+        "Nombre": "Estudiar o No Una Maestría",
+        "URL": '1urLlPxHbT0F7ltTFpk1BN',
+    },
+    {
+        "Id": 'T2E3',
+        "Nombre": 'Control de Almacén Vs Control de Inventario',
+        "URL": '5S2ltNltHx7mvPDimJvuLp',
+    },
+    {
+        "Id": 'T2E4',
+        "Nombre": 'Todos Debemos de Aprender a Programar',
+        "URL": '6AMIkDTI5I82iYVVu3cRyA'
+    },
+    {
+        "Id": 'T2E5',
+        "Nombre": 'Logística y Cadena de Suministro con Lucy González',
+        "URL": '4rsPCAa3qQY1Vfs6TNgpKz',
+    },
+    {
+        "Id": 'T2E6',
+        "Nombre": 'Hablemos de RPA',
+        "URL": '2yJjYWBL8slc0ivrivG4ni'
+    },
+    {
+        "Id": 'T2E7',
+        "Nombre": 'Hablemos de Blockchain',
+        "URL": '4d1KC18N58lwmO2jYuWOCc',
     }
-]
+];
+
+// Replaces 
+function repSp(Id) {
+    // const arrayName = String(Id).substring(0, 4);
+    // Id = String(Id).substring(5);
+    const auxObj = sp.filter(episodio => episodio.Id == Id);
+    var block = document.querySelector(".selector");
+    block.outerHTML = '<iframe style="border-radius:12px"' + block.innerHTML + 'picture-in-picture"></iframe>';
+    block.outerHTML.replace('{{episode}}', auxObj[0].URL);
+    // document.body.innerHTML = document.body.innerHTML.replace('{{episode}}', auxObj[0].URL);
+}
